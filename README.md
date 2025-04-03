a476e2a775eb623d74790bcef625e1867bab6dbf
Versile Johnson II <versilej@gmail.com>
2025-04-03 13:37:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# RepositoryTemplate
+General-use template for new project repositories
+
+### Structure Definition
+* `docs` - Project documentation
+* `src` - All project source code. This should include your csproj file(s) and directory structure(s) for the product
+* `tests` - Unit, integration, acceptance, etc test code
---
+++ tests/MudX.UnitTests/BunitTest.cs
@@ -0,0 +1,38 @@
+﻿using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using NUnit.Framework;
+
+namespace MudX.UnitTests
+{
+    public abstract class BunitTest
+    {
+        protected Bunit.TestContext Context { get; private set; } = null!;
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            Context = new();
+            Context.AddTestServices();
+        }
+
+        [TearDown]
+        public void TearDown() => Context.Dispose();
+    }
+
+    public static class TestContextExtensions
+    {
+        public static void AddTestServices(this Bunit.TestContext ctx)
+        {
+            ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+            ctx.Services.AddMudServices(options =>
+            {
+                options.SnackbarConfiguration.ShowTransitionDuration = 0;
+                options.SnackbarConfiguration.HideTransitionDuration = 0;
+                options.PopoverOptions.CheckForPopoverProvider = false;
+            });
+            ctx.Services.AddScoped(sp => new HttpClient());
+            ctx.Services.AddOptions();
+        }
+    }
+}
