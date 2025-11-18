91b2c9a365d4030012121b448070d9215595e930
Versile Johnson II <versilej@gmail.com>
2025-11-18 11:55:58 -0600

    component port from MudBlazor

---
+++ src/MudX/Components/MudXChat/MudXChatFooter.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace MudX
+{
+    /// <summary>
+    /// Represents the footer of a <see cref="MudChat"/>.
+    /// </summary>
+    public partial class MudXChatFooter : MudComponentBase
+    {
+        /// <inheritdoc />
+        protected string Classname => new CssBuilder("mud-chat-footer")
+            .AddClass(Class)
+            .Build();
+
+        /// <summary>
+        /// The time to display within this header.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Chat.Appearance)]
+        public string? Text { get; set; }
+
+        /// <summary>
+        /// Child content of component.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Chat.Behavior)]
+        public RenderFragment? ChildContent { get; set; }
+    }
+}
